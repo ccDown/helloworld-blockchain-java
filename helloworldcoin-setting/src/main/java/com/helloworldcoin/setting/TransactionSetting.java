8009555150fb4ab5e29059f183b984c73745c734
x.king <xdotking@gmail.com>
2022-08-16 15:30:25 +0800

    2022081606

--- helloworldcoin-setting/src/main/java/com/helloworldcoin/setting/TransactionSetting.java
+++ helloworldcoin-setting/src/main/java/com/helloworldcoin/setting/TransactionSetting.java
@@ -7,6 +7,6 @@ package com.helloworldcoin.setting;
  */
 public class TransactionSetting {
 
-    //max character count of transaction
+    //max character count of transaction //todo 1024 ?
     public static final long TRANSACTION_MAX_CHARACTER_COUNT = 8888;
 }
